36508ad2b24ea59fa20c97c5b58ff99e0ba106ea
isiagi <isiagigeofrey0@gmail.com>
2021-07-28 02:51:23 +0300

    refactors code

--- server.js
+++ server.js
@@ -14,22 +14,19 @@ app.use(cors())
 
 app.get('/photos', async(req, res) => {
     const albumId = req.query.albumId
-    redisClient.get(`photos?albumId=${albumId}`, async (error, photos) => {
-        if (error) console.error(error)
-        if(photos != null){
-            return res.json(JSON.parse(photos))
-        }else{
-            const { data } = await axios.get("https://jsonplaceholder.typicode.com/photos", {params: {albumId}})
-            redisClient.setex(`photos?albumId=${albumId}`, defaultExpiration, JSON.stringify(data));
-            res.json(data)
-        }
+    const photos = await getOrSetCache(`photos?albumId=${albumId}`, async() => {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/photos", {params: {albumId}})
+        return data
     })
-   
+   res.json(photos)
 })
 
 app.get('/photos/:id', async(req, res) => {
-    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/photos/${req.params.id}`)
-    res.json(data)
+    const photos = await getOrSetCache(`photos:${req.params.id}`, async() => {
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/photos/${req.params.id}`)
+        return data
+    })
+   res.json(photos)
 })
 
 function getOrSetCache(key, cb){
